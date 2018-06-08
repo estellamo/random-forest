e1fcc53c8ea572f6110483ff8fd08c1f653f1e21
johnnysclai <25567299+johnnysclai@users.noreply.github.com>
2018-06-08 14:32:56 +0800

    Update

--- main.py
+++ main.py
@@ -8,9 +8,21 @@ Created on Mon Jun  4 10:56:26 2018
 import scipy.io as sio # 用来读取matlab数据
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import cross_validation
+import numpy as np
+
+
+def nFold(n):
+    folds = []
+    base = list(range(n))
+    for i in range(n):
+        test = [i]
+        train = list(set(base) - set(test))
+        folds.append([train, test])
+    return folds
+
 
 #matlab 文件名
-matfn = 'E:/supplement/features_EYE_W2_LPQ_TOP_180406.mat'
+matfn = './data/features_EYE_W2_LPQ_TOP_180406.mat'
 #matfn = 'E:/supplement/features_EYE_W3_LPQ_TOP_180406.mat'
 #matfn = 'E:/supplement/features_EYE_W23_LPQ_TOP_180406.mat'
 #matfn = 'E:/supplement/features_MOUTH_W2_LPQ_TOP_180406.mat'
@@ -19,18 +31,32 @@ matfn = 'E:/supplement/features_EYE_W2_LPQ_TOP_180406.mat'
 data = sio.loadmat(matfn)
 #data2 = sio.loadmat(matfn2)
 
-x = data.get('fea')
-y = data.get('labelValue')
-y1 = y.ravel() #change column vector to an array
+x_total = data.get('fea')
+targets_total = data.get('labelValue')
+labels_total = data.get('labelFeedback')
+subject_labels = data.get('labelSubject')
+subject_unique = np.unique(subject_labels)
 
-#split train set and validation set
-train_x, test_x, train_y, test_y = cross_validation.train_test_split(x, y, random_state = 0)
-# classification decision tree
-clf = RandomForestClassifier(n_estimators = 100, criterion = 'gini')
+# LOSO per subject
+for subject in subject_unique:
+    mask = subject_labels.ravel() == subject
 
-#训练模型
-s = clf.fit(train_x,train_y)
+    # select the features, targets, labels from a subject
+    x = x_total[mask]
 
-r = clf.score(test_x,test_y)
-print (r)
-prob_predict_test_y = clf.predict_proba(test_x)
+    targets = targets_total[mask]
+    labels = labels_total[mask]
+    num_sample = len(x)
+    folds = nFold(n = num_sample)
+    for train, test in folds:
+        # Split the train set and test set
+        train_x, train_targets, train_labels = x[train], targets[train], labels[train]
+        test_x, test_targets, test_labels = x[test], targets[test], labels[test]
+
+        # Train random forest
+        clf = RandomForestClassifier(n_estimators=100, criterion='gini', n_jobs=-1)
+        clf.fit(train_x, train_targets.ravel())
+
+        # Evaluation
+        predict_prob = clf.predict_proba(test_x)
+        print('Predict: {} Target: {}'.format(predict_prob, test_targets))
